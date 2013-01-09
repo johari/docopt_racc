77077baf6114534de0c9cd777a03b0ecd1031233
Nima Johari <nimajohari@gmail.com>
2013-01-09 10:52:52 +0330

    OMG COLORZ!!

--- script/run_docopt_test.rb
+++ script/run_docopt_test.rb
@@ -1,5 +1,7 @@
 require "turn"
 require "yaml"
+require "colored"
+require "json"
 require File.expand_path("../../lib/docopt.rb", __FILE__)
 
 require 'optparse'
@@ -26,12 +28,24 @@ tests_path = File.expand_path("../../test/test_cases.yaml", __FILE__)
 test_cases = YAML::load(File.open(tests_path).read)
 YAML::load(suite_yaml_result).cases.each do |kase|
   kase.tests.sort_by { |x| x.name }.each do |test|
-    test_name = test.name.gsub /^test_/, ''
-    test_name = test_name.gsub /_run_.*$/, ''
-    if test.pass? then
-      puts "PASSED %s" % test_name
-    elsif test.fail? then
-      puts "FAILED %s" % test_name
+    if options[:choose] and !test.name.match Regexp.new(options[:choose]) then
+      next
+    end
+    puts "xxx".green.bold * 10 if options[:choose]
+    matches = test.name.split "_run_"
+    run_id = matches[1]
+    test_name = matches[0].gsub /^test_/, ''
+    status = test.pass? ? "PASSED".green.bold : "FAILED".red.bold
+    test_banner = test_name.yellow
+    test_banner += " run " + run_id.to_s.blue.bold if run_id
+    puts "#{status} #{test_banner}"
+
+    if options[:choose] and !test.pass? then
+      r = test_cases[test_name]["runs"][run_id.to_i]
+      puts ("EXPECTED: " + r["expect"].to_json).magenta.bold
+      puts ("MESSGAGE: ".red.bold) + test.message.magenta.bold
+      puts "ARGS : ".red.bold + ("prog %s" % r["prog"]).to_s.blue.bold
+      puts test_cases[test_name]["usage"].yellow.bold
     end
   end
 end
