901652275bc3741105a0f96c5e0a0f5866842242
Nima Johari <nimajohari@gmail.com>
2013-01-13 00:25:08 +0330

    add new usage_block lexer

---
+++ lib/docopt/usage_block/lexer.rb
@@ -0,0 +1,107 @@
+module Docopt
+  module UsageBlock
+    class Lexer
+      require 'strscan'
+
+      SHORT_OPT = /-[a-z]/i
+      LONG_OPT = /--[a-z][a-z-]+/i
+      DELIM = /(, *)| +/
+      ARG = /<[a-z]+>|[A-Z]+/
+      LDOTS = /\.\.\./
+
+      def initialize str
+        @ss = StringScanner.new(str)
+        @state = nil
+        @prog_name = nil
+
+        @ignore_rest_of_line_ret = nil
+      end
+
+      def next_token
+        return nil if @ss.eos?
+
+        case @state
+        when nil
+          @ss.skip_until(/^usage:\n?/i)
+          @state = :prev_or_new?
+          next_token
+        when :help_message
+          help_line = @ss.scan(/.+/)
+          if help_line =~ /\[default: *(.*)\]/i then
+            [:t_default, $1]
+          else
+            if text = @ss.scan(/\n/) or @ss.eos? then
+              @state = :prev_or_new?
+              next_token
+            else
+              raise "me"
+            end
+          end
+        when :new_car
+          @ss.skip(/[[:blank:]]+/)
+          if text = @ss.scan(/#{Regexp.quote @prog_name}/) then
+            @state = :new_cdr
+            [:t_prog_name, text]
+          else
+          end
+        when :short_stack
+          if text = @ss.scan(/[a-z]/) then
+            [:t_short_opt, "-#{text}"]
+          else
+            @state = :new_cdr
+            next_token
+          end
+        when :new_cdr
+          @ss.skip(/[[:blank:]]+/)
+          if text = @ss.scan(/\[options\]/) then
+            [:t_options_shorthand, text]
+          elsif text = @ss.scan(/\[/) then
+            [:t_lbrace, text]
+          elsif text = @ss.scan(/\]/) then
+            [:t_rbrace, text]
+          elsif text = @ss.scan(/\(/) then
+            [:t_lparen, text]
+          elsif text = @ss.scan(/\)/) then
+            [:t_rparen, text]
+          elsif text = @ss.scan(/-[a-z]/) then
+            @state = :short_stack
+            [:t_short_opt, text]
+          elsif text = @ss.scan(/[a-z]+/) then
+            [:t_cmd, text]
+          elsif text = @ss.scan(LDOTS) then
+            [:t_ldots, text]
+          elsif text = @ss.scan(LONG_OPT) then
+            [:t_long_opt, text]
+          elsif text = @ss.scan(/\=/) then
+            [:t_eq, text]
+          elsif text = @ss.scan(ARG) then
+            [:t_arg, text]
+          elsif text = @ss.scan(/\|/) then
+            [:t_or, text]
+          elsif text = @ss.scan(/\n/) then
+            @state = :prev_or_new?
+            next_token
+          end
+        when :prev_or_new?
+          @ss.skip(/[[:blank:]]+/)
+          if @prog_name.nil? then
+            @prog_name = @ss.scan(/[\w\._-]+/)
+            @ss.unscan
+            return next_token
+          end
+          if (text = @ss.scan(/#{Regexp.quote @prog_name}/)) then
+            @state = :new_car
+            @ss.unscan
+            [:t_synopses_begin, nil]
+          elsif text = @ss.scan(/\n/) then
+            @ss.terminate
+            next_token
+          else
+            @state = :new_cdr
+            next_token
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test/usage_block_lexer_test.rb
@@ -0,0 +1,103 @@
+require "minitest/autorun"
+require "test_helper"
+require "docopt/usage_block/lexer"
+
+module Docopt
+  module UsageBlock
+    class TestUsageBlockLexer < MiniTest::Unit::TestCase
+      def tokens_for str
+        lexer = Lexer.new str
+        res = []
+        while (t = lexer.next_token) do
+          res.push t
+        end
+        res
+      end
+
+      def test_raw_naval_fate
+        expected = [[:t_synopses_begin, nil],
+                    [:t_prog_name, "naval_fate.py"],
+                      [:t_cmd, "ship"], [:t_cmd, "new"],
+                      [:t_arg, "<name>"], [:t_ldots, "..."],
+                    [:t_synopses_begin, nil],
+                      [:t_prog_name, "naval_fate.py"],
+                      [:t_cmd, "ship"], [:t_arg, "<name>"],
+                      [:t_cmd, "move"], [:t_arg, "<x>"], [:t_arg, "<y>"],
+                      [:t_lbrace, "["],
+                        [:t_long_opt, "--speed"],
+                        [:t_eq, "="], [:t_arg, "<kn>"],
+                      [:t_rbrace, "]"],
+                    [:t_synopses_begin, nil],
+                      [:t_prog_name, "naval_fate.py"],
+                      [:t_cmd, "ship"],
+                      [:t_cmd, "shoot"], [:t_arg, "<x>"], [:t_arg, "<y>"],
+                    [:t_synopses_begin, nil],
+                      [:t_prog_name, "naval_fate.py"],
+                      [:t_cmd, "mine"],
+                      [:t_lparen, "("],
+                        [:t_cmd, "set"], [:t_or, "|"], [:t_cmd, "remove"],
+                      [:t_rparen, ")"], [:t_arg, "<x>"], [:t_arg, "<y>"],
+                      [:t_lbrace, "["],
+                        [:t_long_opt, "--moored"],
+                        [:t_or, "|"], [:t_long_opt, "--drifting"],
+                      [:t_rbrace, "]"],
+                    [:t_synopses_begin, nil],
+                      [:t_prog_name, "naval_fate.py"],
+                        [:t_short_opt, "-h"],
+                        [:t_or, "|"], [:t_long_opt, "--help"],
+                    [:t_synopses_begin, nil],
+                      [:t_prog_name, "naval_fate.py"],
+                      [:t_long_opt, "--version"]]
+
+        assert_equal expected, tokens_for(load_raw "naval_fate")
+      end
+
+      def test_raw_options
+        expected = [[:t_synopses_begin, nil],
+                    [:t_prog_name, "options_example.py"],
+                      [:t_lbrace, "["],
+                        [:t_short_opt, "-h"],
+                        [:t_short_opt, "-v"],
+                        [:t_short_opt, "-q"],
+                        [:t_short_opt, "-r"],
+                        [:t_short_opt, "-f"],
+                        [:t_arg, "NAME"],
+                      [:t_rbrace, "]"],
+                      [:t_lbrace, "["],
+                        [:t_long_opt, "--exclude"],
+                        [:t_eq, "="], [:t_arg, "PATTERNS"],
+                      [:t_rbrace, "]"],
+                      [:t_lbrace, "["],
+                        [:t_long_opt, "--select"],
+                        [:t_eq, "="], [:t_arg, "ERRORS"],
+                        [:t_or, "|"], [:t_long_opt, "--ignore"],
+                        [:t_eq, "="], [:t_arg, "ERRORS"],
+                      [:t_rbrace, "]"],
+                      [:t_lbrace, "["],
+                        [:t_long_opt, "--show-source"],
+                      [:t_rbrace, "]"],
+                      [:t_lbrace, "["],
+                        [:t_long_opt, "--statistics"],
+                      [:t_rbrace, "]"],
+                      [:t_lbrace, "["],
+                        [:t_long_opt, "--count"],
+                      [:t_rbrace, "]"],
+                      [:t_lbrace, "["],
+                        [:t_long_opt, "--benchmark"],
+                      [:t_rbrace, "]"],
+                      [:t_arg, "PATH"], [:t_ldots, "..."],
+                    [:t_synopses_begin, nil],
+                    [:t_prog_name, "options_example.py"],
+                      [:t_lparen, "("],
+                        [:t_long_opt, "--doctest"], [:t_or, "|"],
+                        [:t_long_opt, "--testsuite"],
+                        [:t_eq, "="], [:t_arg, "DIR"],
+                      [:t_rparen, ")"],
+                    [:t_synopses_begin, nil],
+                    [:t_prog_name, "options_example.py"],
+                      [:t_long_opt, "--version"]]
+          assert_equal expected, tokens_for(load_raw "options_example")
+      end
+    end
+  end
+end
